7360787c70fcda921dc289e129d65680e20b5f97
Polishchuk Olexandra <117304306+Slavyasanek@users.noreply.github.com>
2024-07-03 17:25:37 +0300

    update hero

--- src/assets/js/modules/sliders.js
+++ src/assets/js/modules/sliders.js
@@ -1,15 +1,8 @@
 // core version + navigation, pagination modules:
-import 'swiper/swiper-bundle.css';
-import Swiper from 'swiper'
-import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import Swiper from 'swiper';
 
 // init Swiper:
 new Swiper('#homeScreenSlider', {
-  // configure Swiper to use modules
-  modules: [Navigation],
-  // Navigation arrows
-  navigation: {
-    nextEl: '#homeScreenSlider-next',
-    prevEl: '#homeScreenSlider-prev',
-  },
+
 })
